e81bc1f309f3c5ef85ace19f42f2856426e8e760
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-01-09 21:24:47 -0700

    routes of customer profile

--- backend/src/routes/customerAccountRoutes.ts
+++ backend/src/routes/customerAccountRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express'
-import { withdrawMoney, depositMoney, transferMoney, getAccountDetails } from '../controllers/customerAccountController';
+import { createAccount, withdrawMoney, depositMoney, transferMoney, getAccountDetails, viewCustomerProfile , viewAccountBalance } from '../controllers/customerAccountController';
 
 const router = express.Router();
+router.post('/create', createAccount);
 router.post('/deposit', depositMoney)
 router.post('/withdraw', withdrawMoney)
 router.post('/transfer', transferMoney)
 router.get('/:accountNumber', getAccountDetails)
+router.get('/profile/:accountNumber', viewCustomerProfile); 
+router.get('/balance/:accountNumber', viewAccountBalance);
 
 export default router;
