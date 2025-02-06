bd1ab287d8bedf7b6dedfbf8fef9b55fad15165a
GITOLI Remy Claudien <gitoliremy@gmail.com>
2025-02-05 21:51:11 -0700

    title added

--- frontend/src/pages/CreateAccount.js
+++ frontend/src/pages/CreateAccount.js
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import {
+     View, 
+     Text, 
+     ScrollView, 
+     StyleSheet,
+     Dimensions, } from 'react-native'
 import React from 'react'
+
+const { width, height } = Dimensions.get('window')
 
 export default function CreateAccount(){
   return (
-    <View>
-      <Text>CreateAccount</Text>
-    </View>
+    <ScrollView style={{ backgroundColor: '#e1e1e1', flex: 1}} contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Create Account</Text>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {alignItems: 'center', width: width, height: height, padding: 46 },
+    title: {fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginBottom: 20}
+})
